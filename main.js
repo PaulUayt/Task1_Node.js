ad12bab1411b7cb66c88e390521dde35d590552c
PaulUayt <pauluayt@gmail.com>
2021-03-11 00:18:19 +0200

    add all files

---
+++ main.js
@@ -0,0 +1,6 @@
+let index = require('./node_modules/math/index.js')
+
+console.log("abs: " + index.abs(-5))
+let arr1 = [0,456,45,-6,-1,0]
+console.log("min: "index.min(arr1))
+console.log("sum: "index.summ(4,-15))
